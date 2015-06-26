693047fcd29da93fb13ef67aea0a31e9855172cb
erikvaningen <erikvaningen@gmail.com>
2015-06-26 11:54:22 +0200

    developing tests

--- figis-factsheets/src/test/java/org/fao/fi/figis/fs/common/business/FsSearchServiceTest.java
+++ figis-factsheets/src/test/java/org/fao/fi/figis/fs/common/business/FsSearchServiceTest.java
@@ -1,10 +1,12 @@
 package org.fao.fi.figis.fs.common.business;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -49,15 +51,36 @@ public class FsSearchServiceTest {
 	}
 
 	/**
+	 * [dslist=fishery., nolimit=., kw0=georeference., kv0=myanmar., kl0=., logop=., lang=., xsl=.,
+	 * lixsl=webapps/figis/firms/format/searchfsfirmslist.xsl., refxml=false., octrl=., dom=., syn=., draft=.]
 	 * 
-	 * [dslist=., nolimit=., kw0=., kv0=trawl., kl0=., logop=., lang=., xsl=.,
-	 * lixsl=., refxml=false., octrl=., dom=., syn=., draft=.]
+	 * @throws Exception
+	 */
+	@Test
+	public void testPerformSearchGeoreportMyanmar() throws Exception {
+
+		FiServiceParamHolder fiServiceParamHolder = new FiServiceParamHolder();
+		fiServiceParamHolder.addParam(new FiServiceRequestParam("dslist", "fishery"));
+		fiServiceParamHolder.addParam(new FiServiceRequestParam("kw0", "georeference"));
+		fiServiceParamHolder.addParam(new FiServiceRequestParam("kv0", "myanmar"));
+		fiServiceParamHolder.addParam(new FiServiceRequestParam("refxml", "false"));
+		fiServiceParamHolder.addParam(new FiServiceRequestParam("lixsl",
+				"webapps/figis/firms/format/searchfsfirmslist.xsl"));
+
+		Document document = fsSearchService.performSearch(fiServiceParamHolder);
+		assertNotNull(document);
+		document2File(document);
+		assertTrue(document2String(document).contains("Myanmar"));
+	}
+
+	/**
 	 * 
-	 * [0] FiServiceRequestParam (id=96) mLog Logger (id=104) mName "dslist"
-	 * (id=107) mType 0 mValues String[1] (id=112)
+	 * [dslist=., nolimit=., kw0=., kv0=trawl., kl0=., logop=., lang=., xsl=., lixsl=., refxml=false., octrl=., dom=.,
+	 * syn=., draft=.]
 	 * 
-	 * dslist=.&nolimit=.&kw0=.&kv0=trawl.&kl0=.&logop=.&lang=.&xsl=.&lixsl=.&
-	 * refxml=false.&octrl=.&dom=.&syn=.&draft=.
+	 * [0] FiServiceRequestParam (id=96) mLog Logger (id=104) mName "dslist" (id=107) mType 0 mValues String[1] (id=112)
+	 * 
+	 * dslist=.&nolimit=.&kw0=.&kv0=trawl.&kl0=.&logop=.&lang=.&xsl=.&lixsl=.& refxml=false.&octrl=.&dom=.&syn=.&draft=.
 	 * 
 	 * 
 	 */
@@ -69,7 +92,7 @@ public class FsSearchServiceTest {
 		fiServiceParamHolder.setParameter(p);
 		Document document = fsSearchService.performSearch(fiServiceParamHolder);
 		assertNotNull(document);
-		document2Sysout(document);
+		document2File(document);
 	}
 
 	@Test
@@ -88,11 +111,11 @@ public class FsSearchServiceTest {
 		// um.unmarshal(document);
 
 		assertNotNull(document);
-		document2Sysout(document);
+		document2File(document);
 
 	}
 
-	private void document2Sysout(Document document) throws TransformerException, IOException {
+	private void document2File(Document document) throws TransformerException, IOException {
 		// set up a transformer
 		TransformerFactory transfac = TransformerFactory.newInstance();
 		Transformer trans = transfac.newTransformer();
@@ -106,6 +129,23 @@ public class FsSearchServiceTest {
 		StreamResult result = new StreamResult(fw);
 		DOMSource source = new DOMSource(document);
 		trans.transform(source, result);
+	}
+
+	private String document2String(Document document) throws TransformerException, IOException {
+		// set up a transformer
+		TransformerFactory transfac = TransformerFactory.newInstance();
+		Transformer trans = transfac.newTransformer();
+		trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		trans.setOutputProperty(OutputKeys.INDENT, "yes");
+
+		// create string from xml tree
+
+		StringWriter sw = new StringWriter();
+
+		StreamResult result = new StreamResult(sw);
+		DOMSource source = new DOMSource(document);
+		trans.transform(source, result);
+		return sw.getBuffer().toString();
 	}
 
 	@Test
